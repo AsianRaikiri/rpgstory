79db9f0a3bdb32ae659dbb9dfdbfa01dbecdb110
BuildTools <h.m.grunicke@gmail.com>
2024-09-27 16:08:11 +0200

    added actual name in help command

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
+#! /usr/bin/env node
+
+const { Command } = require("commander");
+const fs = require("fs");
+const path = require("path");
 const figlet = require("figlet");
 
+const program = new Command();
+
 console.log(figlet.textSync("RPG Story - First Edition"));
+
+program
+  .version("1.0.0")
+  .name("rpgstory")
+  .description("The first edition of RPG Story, an interactive rpg game on the CLI")
+  .parse(process.argv);
+
+const options = program.opts();
+
+if (!process.argv.slice(2).length) {
+    program.outputHelp();
+  }
